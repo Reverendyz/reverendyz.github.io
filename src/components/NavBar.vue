f1d69891781ddab45a3260ea442b51172d6055ba
Felipe Moreira dos Santos <fmsantos1997@hotmail.com>
2025-12-26 10:21:07 -0300

    fix: modified navbar component to be responsive and adaptative to mobile screens

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -7,23 +7,22 @@ const theme = useTheme()
 <template>
   <v-app-bar>
     <v-container class="fg-white mx-auto d-flex align-center justify-space-between flex-wrap pa-2">
-      <v-app-bar-title class="text-h6 text-sm-h5"
-        ><router-link to="/" class="text-decoration-none text-white"
-          >RevzIT</router-link
-        ></v-app-bar-title
+      <v-app-bar-title class="text-h6 text-sm-h5 d-none d-sm-flex"
+        ><a href="/" class="text-decoration-none text-white">RevzIT</a></v-app-bar-title
       >
       <div class="d-flex align-center flex-wrap">
         <v-btn to="/" size="small" class="mx-1"> {{ $t('nav.home') }} </v-btn>
         <v-btn to="/about" size="small" class="mx-1"> {{ $t('nav.about') }} </v-btn>
         <v-btn to="/projects" size="small" class="mx-1"> {{ $t('nav.projects') }} </v-btn>
         <v-btn
-          :ripple="false"
           variant="outlined"
-          @click="$i18n.locale = $i18n.locale === 'en-US' ? 'pt-BR' : 'en-US'"
           size="small"
           class="mx-1"
+          @click="$i18n.locale = $i18n.locale === 'en-US' ? 'pt-BR' : 'en-US'"
         >
-          {{ $i18n.locale === 'en-US' ? 'PT-BR' : 'EN-US' }}
+          <span class="d-none d-sm-inline">
+            {{ $i18n.locale === 'en-US' ? 'PT-BR' : 'EN-US' }}
+          </span>
           <v-icon>mdi-translate</v-icon>
         </v-btn>
         <v-btn
